7c66805d3d3a0adec2fc71f4a112059211f7011e
Sakari Laaksonen <sakari.laaksonen@siili.com>
2021-11-02 15:52:01 +0200

    testing env variable scopes in the Docker build and run contexts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/main.css'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+console.log('TEST', process.env.NEXT_PUBLIC_TEST)
 import App from 'next/app'
 export default App
