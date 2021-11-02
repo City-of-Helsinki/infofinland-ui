69b39d69402121578a6b07147b64253f2fc879e7
Sakari Laaksonen <sakari.laaksonen@siili.com>
2021-11-02 16:32:44 +0200

    cleanup and font size fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/main.css'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
-console.log('TEST', process.env.NEXT_PUBLIC_TEST)
 import App from 'next/app'
 export default App
