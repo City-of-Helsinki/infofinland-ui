25007d0c36abb639f398e039839906ead1e3c102
Sakari Laaksonen <sakari.laaksonen@siili.com>
2021-11-30 10:47:00 +0200

    add animation to cookie banner

--- src/components/layout/CookieConsent.jsx
+++ src/components/layout/CookieConsent.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { isCookieConsentSet, cookieConsent } from '@/src/store'
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
 import { useRouter } from 'next/router'
+import { CSSTransition } from 'react-transition-group'
 
 export function CookieConsentActions() {
   const { t } = useTranslation('common')
@@ -40,24 +41,48 @@ export default function CookieConsentBar() {
     null
 
   return (
-    !isConsentSet &&
     !isAboutPage && (
-      <section className="sticky bottom-0 items-center px-6 lg:px-16 pt-6 lg:pt-12 text-center lg:text-left bg-white rounded-t shadow-bottom-bar min-bottom-bar lg:rounded-none">
-        <h2 className="mb-4 text-body-large font-bold">{t('cookies.title')}</h2>
-        <div className="lg:flex lg:gap-12 pb-6 max-w-topbar">
-          <div className="lg:w-1/2">
-            <p className="mb-8 text-action lg:text-body">{t('cookies.text')}</p>
+      <CSSTransition
+        classNames={{
+          appear: 'ifu-cookies__banner--appear',
+          appearActive: 'ifu-cookies__banner--appear-active',
+          appearDone: 'ifu-cookies__banner--appear-done',
+          enter: 'ifu-cookies__banner--enter',
+          enterActive: 'ifu-cookies__banner--enter-active',
+          enterDone: 'ifu-cookies__banner--enter-done',
+          exit: 'ifu-cookies__banner--exit',
+          exitActive: 'ifu-cookies__banner--exit-active',
+          exitDone: 'ifu-cookies__banner--exit-done',
+        }}
+        timeout={{ appear: 0, enter: 300, exit: 200 }}
+        in={!isConsentSet}
+        mountOnEnter
+        appear
+        unmountOnExit
+      >
+        <section className="sticky bottom-0 items-center px-6 lg:px-16 pt-6 lg:pt-12 text-center lg:text-left bg-white rounded-t shadow-bottom-bar min-bottom-bar lg:rounded-none">
+          <h2 className="mb-4 text-body-large font-bold">
+            {t('cookies.title')}
+          </h2>
+          <div className="lg:flex lg:gap-12 pb-6 max-w-topbar">
+            <div className="lg:w-1/2">
+              <p className="mb-8 text-action lg:text-body">
+                {t('cookies.text')}
+              </p>
+            </div>
+            <div className="gap-4 justify-items-center items-center">
+              <CookieConsentActions />
+              {!isConsentSet && (
+                <Link passHref href={process.env.COOKIE_PAGE_PATH}>
+                  <a className="font-bold text-black underline">
+                    {t('cookies.readMore')}
+                  </a>
+                </Link>
+              )}
+            </div>
           </div>
-          <div className="gap-4 justify-items-center items-center">
-            <CookieConsentActions />
-            <Link passHref href={process.env.COOKIE_PAGE_PATH}>
-              <a className="font-bold text-black underline">
-                {t('cookies.readMore')}
-              </a>
-            </Link>
-          </div>
-        </div>
-      </section>
+        </section>
+      </CSSTransition>
     )
   )
 }
