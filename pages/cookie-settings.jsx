75ffcf555dbf708de7451e556ab33a88b26eadcb
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-01-13 11:37:58 +0200

    disable CityMenu if there are no municipalities

--- pages/cookie-settings.jsx
+++ pages/cookie-settings.jsx
@@ -15,23 +15,20 @@ export async function getStaticProps(context) {
   return {
     props: {
       ...(await serverSideTranslations(context.locale, ['common'])),
-      ...(await getCommonApiContent(
-        context,
-        serverRuntimeConfig.DRUPAL_MENUS.ABOUT
-      )),
+      ...(await getCommonApiContent(context)),
     },
     revalidate: serverRuntimeConfig.REVALIDATE_TIME,
   }
 }
 
-export default function CookieConsentPage({ ...layout }) {
+export default function CookieConsentPage(props) {
   const { t } = useTranslation('common')
   const isAnalyticsAllowed = useAtomValue(cookieConsent)
   const setConsent = useUpdateAtom(cookieConsent)
   const toggleConsent = () => setConsent(!isAnalyticsAllowed)
 
   return (
-    <AboutPage {...layout}>
+    <AboutPage {...props}>
       <Block about>
         <h1 className="mt-16 mb-8 text-h1 lg:text-h1xl">
           {t('cookies.settings.title')}
