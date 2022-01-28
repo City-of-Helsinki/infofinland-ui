6c95f74bf749f9d2645d8d99caf6e10ba995b016
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-01-28 14:01:23 +0200

    remove debug line, test accordion video. It works with existing queries

--- src/hooks/useRouterWithLocalizedPath.js
+++ src/hooks/useRouterWithLocalizedPath.js
@@ -4,9 +4,7 @@ export default function useRouterWithLocalizedPath() {
   const router = useRouter()
   const { asPath, locale } = router
   const localePathWithHash = `/${locale}${asPath}`
-
   const localePath = /#/.test(localePathWithHash) ? localePathWithHash.split('#').at(0) : localePathWithHash
   const hash = localePathWithHash.split('#').at(1)
-  console.log({localePathWithHash, localePath,hash})
   return { localePath, localePathWithHash, hash,  ...router }
 }
