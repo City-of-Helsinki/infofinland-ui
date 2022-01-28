87249a315c5a3c1d4c125fc9351e80fbc73256ae
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-01-28 13:17:24 +0200

    remove dev notes

--- pages/api/localinfo.js
+++ pages/api/localinfo.js
@@ -5,8 +5,7 @@ import { getLocalInfoParams } from '@/lib/query-params'
 export default async function handler(req, res) {
   const { query } = req
   const { uuid, name: city } = query
-  // Mocking empty results for page testing
-  // Change test implementation when we have real search API
+
   let status = 200
   const node = await getResource(NODE_TYPES.PAGE, uuid, {
     params: getLocalInfoParams(),
