1d868842afda7a73fbd392e354cf067f89f739ce
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-04-08 16:55:25 +0300

    add CACHE_REPOPULATE=1 to Docker runner

--- src/lib/server-cache.js
+++ src/lib/server-cache.js
@@ -1,6 +1,6 @@
 import NodeCache from 'node-cache'
 const stdTTL = process.env.NODE_ENV !== 'production' ? 10 : 60
-const checkperiod = process.env.NODE_ENV !== 'production' ? 30 : 60
+const checkperiod = process.env.NODE_ENV !== 'production' ? 30 : 45
 const cache = new NodeCache({ stdTTL, checkperiod })
 export const MENU_CACHE_TTL = 120
 export default cache
