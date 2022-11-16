1120653929eb1b73eddea9036d937dbb91446660
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-11-16 12:17:50 +0200

    new drupal client in use

--- src/lib/drupal-client.js
+++ src/lib/drupal-client.js
@@ -1,21 +1,23 @@
 import { DrupalClient } from 'next-drupal'
 import getConfig from 'next/config'
 export const getDrupalClient = (withAuth) => {
-  const { NEXT_PUBLIC_DRUPAL_BASE_URL, DRUPAL_PREVIEW_SECRET, DRUPAL_CLIENT_ID, DRUPAL_CLIENT_SECRET } = getConfig().serverRuntimeConfig
-  return new DrupalClient(
+  const {
     NEXT_PUBLIC_DRUPAL_BASE_URL,
-    {
-      auth: {
-        clientId: DRUPAL_CLIENT_ID,
-        clientSecret: DRUPAL_CLIENT_SECRET
-      },
-      //cache: redisCache,
-      previewSecret: DRUPAL_PREVIEW_SECRET,
-      // Make preview work in development environment.
-      forceIframeSameSiteCookie: process.env.NODE_ENV === 'development',
-      ...(withAuth && { withAuth: true })
-    }
-  )
+    DRUPAL_PREVIEW_SECRET,
+    DRUPAL_CLIENT_ID,
+    DRUPAL_CLIENT_SECRET,
+  } = getConfig().serverRuntimeConfig
+  return new DrupalClient(NEXT_PUBLIC_DRUPAL_BASE_URL, {
+    auth: {
+      clientId: DRUPAL_CLIENT_ID,
+      clientSecret: DRUPAL_CLIENT_SECRET,
+    },
+    //cache: redisCache,
+    previewSecret: DRUPAL_PREVIEW_SECRET,
+    // Make preview work in development environment.
+    forceIframeSameSiteCookie: process.env.NODE_ENV === 'development',
+    ...(withAuth && { withAuth: true }),
+  })
 }
 
 export default getDrupalClient
