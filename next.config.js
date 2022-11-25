3e1d8ab7831255b2fb6491e7d5838476c772f25c
Sakari Laaksonen <sakari.laaksonen@siili.com>
2022-11-25 12:59:06 +0200

    add redirect to somalian frontpage since it responds to default locale instead

--- next.config.js
+++ next.config.js
@@ -86,6 +86,16 @@ const config = {
     config.module.rules.push({ test: /\.xml$/, loader: 'xml-loader' })
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: '/so',
+        locale: false,
+        destination: '/so/not-supported',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = withBundleAnalyzer(config)
