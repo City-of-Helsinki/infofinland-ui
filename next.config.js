065adcff06c707aa4c28b6987494a0cd7cf0a1f5
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-12-17 10:35:20 +0200

    Simplify next.js settings

--- next.config.js
+++ next.config.js
@@ -142,11 +142,7 @@ module.exports = withSentryConfig(module.exports, {
     disable: !process.env.SENTRY_AUTH_TOKEN,
   },
 
-  // Only print logs for uploading source maps in CI
   silent: false,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: false,
@@ -162,10 +158,4 @@ module.exports = withSentryConfig(module.exports, {
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 })
