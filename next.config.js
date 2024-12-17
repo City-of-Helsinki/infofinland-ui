3cb7ed8aa4efe911d887986c8e0a87f69e7fb97e
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-12-17 08:53:59 +0200

    Simplify config

--- next.config.js
+++ next.config.js
@@ -132,19 +132,7 @@ const config = {
 module.exports = withBundleAnalyzer(config)
 module.exports = withSentryConfig(module.exports, {
   // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-  org: 'city-of-helsinki',
-  project: 'infofinland-ui',
-  //sentryUrl: 'https://sentry.hel.fi/',
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
 
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
